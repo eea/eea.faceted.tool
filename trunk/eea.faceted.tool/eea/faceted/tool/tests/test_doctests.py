60a17cccc3c8588142502691ea7513fb2def1b80
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-07-28 13:49:12 +0000

    #2578   - Buildbot: Fix random fail with UnicodeDecodeError in browser.txt

--- trunk/eea.faceted.tool/eea/faceted/tool/tests/test_doctests.py
+++ trunk/eea.faceted.tool/eea/faceted/tool/tests/test_doctests.py
@@ -1,14 +1,13 @@
 """ Doc tests
 """
-import doctest
 import unittest
+from zope.testing import doctest
 from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
 from base import FacetedFunctionalTestCase
 
 OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
-
 
 def test_suite():
     """ Suite
