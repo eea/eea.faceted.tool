ef90db74f67b5a09db5b9bffac97ae7a5c6c4f54
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-11-02 11:22:44 +0000

    #2578   - Fix package to work with Plone 3.3.x.

--- trunk/eea.faceted.tool/eea/faceted/tool/__init__.py
+++ trunk/eea.faceted.tool/eea/faceted/tool/__init__.py
@@ -1,13 +1,17 @@
 from zope.interface import providedBy
 from Products.CMFCore.utils import ToolInit
 from eea.faceted.tool.tool import FacetedTool
-from Products.CMFPlone.CatalogTool import _eioRegistry
 
-def object_provides(object, portal, **kw):
-    return [i.__identifier__ for i in providedBy(object).flattened()]
+# Plone 2.5 Backward compatible
+try:
+    from Products.CMFPlone.CatalogTool import _eioRegistry
+    def object_provides(object, portal, **kw):
+        return [i.__identifier__ for i in providedBy(object).flattened()]
 
-if not _eioRegistry.has_key('object_provides'):
-    _eioRegistry.register('object_provides', object_provides)
+    if not _eioRegistry.has_key('object_provides'):
+        _eioRegistry.register('object_provides', object_provides)
+except ImportError, err:
+    pass
 
 def initialize(context):
     """ initialize function called when used as a zope2 product """
