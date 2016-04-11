0e8f38b74328111433eb23a0e1b376575f3d52bc
ichim-david <ichim.david@gmail.com>
2016-04-11 18:21:11 +0300

    Feature: switched to z3c.form and added edit form for existing portal_types within portal_faceted tool [ichimdav refs #70786]

--- eea/faceted/tool/browser/app/portaltype.py
+++ eea/faceted/tool/browser/app/portaltype.py
@@ -1,15 +1,18 @@
 """ Portal type forms
 """
-from zope.formlib.form import Fields, PageAddForm, PageEditForm, applyChanges
+from z3c.form import form, field
+from z3c.form import button
 
 from zope.container.interfaces import INameChooser
+from eea.faceted.tool.config import EEAMessageFactory as _
 from eea.faceted.tool.interfaces import IPortalType
 from eea.faceted.tool.storage.portaltype import PortalType
 
-class AddPage(PageAddForm):
+
+class AddPage(form.AddForm):
     """ Add page
     """
-    form_fields = Fields(IPortalType)
+    fields = field.Fields(IPortalType)
 
     def create(self, data):
         """ Create
@@ -17,7 +20,7 @@ class AddPage(PageAddForm):
         name = INameChooser(
             self.context).chooseName(data.get('title', ''), None)
         ob = PortalType(id=name)
-        applyChanges(ob, self.form_fields, data)
+        form.applyChanges(self, ob, data)
         return ob
 
     def add(self, obj):
@@ -33,7 +36,29 @@ class AddPage(PageAddForm):
         """
         return "./@@view"
 
-class EditPage(PageEditForm):
+
+class EditPage(form.EditForm):
     """ Edit page
     """
-    form_fields = Fields(IPortalType)
+    fields = field.Fields(IPortalType)
+
+    def nextURL(self):
+        """ Next
+        """
+        return "../@@view"
+
+    @button.buttonAndHandler(_(u"label_apply", default=u"Apply"),
+                             name='apply')
+    def handleApply(self, action):
+        """ Apply button
+        """
+        super(EditPage, self).handleApply(self, action)
+        self.request.response.redirect(self.nextURL())
+
+    @button.buttonAndHandler(_(u"label_cancel", default=u"Cancel"),
+                             name='cancel_add')
+    def handleCancel(self, action):
+        """ Cancel button
+        """
+        self.request.response.redirect(self.nextURL())
+        return ''
